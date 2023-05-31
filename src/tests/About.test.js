ee78f0ed852419a71b62722cd18683e7b1b03928
Nina Palma <ninapalmadev@gmail.com>
2023-05-31 16:08:05 -0300

    corrige arquivos e faz requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,61 +1,19 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import { About } from '../pages';
 
-describe('Teste o componente <App.js />', () => {
-  it('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-    renderWithRouter(<App />);
+describe('Testa o componente <About.js />', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
 
-    const home = screen.getByRole('link', { name: 'Home' });
-    expect(home).toBeInTheDocument();
-
-    const about = screen.getByRole('link', { name: 'About' });
-    expect(about).toBeInTheDocument();
-
-    const favorite = screen.getByRole('link', { name: 'Favorite Pokémon' });
-    expect(favorite).toBeInTheDocument();
+    const title = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(title).toBeInTheDocument();
   });
 
-  it('Testa se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação', () => {
-    const { history } = renderWithRouter(<App />);
+  it('Testa se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    renderWithRouter(<About />);
 
-    const home = screen.getByRole('link', { name: 'Home' });
-    userEvent.click(home);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-  });
-
-  it('Testa se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação', () => {
-    const { history } = renderWithRouter(<App />);
-
-    const about = screen.getByRole('link', { name: 'About' });
-    userEvent.click(about);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/about');
-  });
-  it('Testa se a aplicação é redirecionada para a página de Pokémon Favoritados, na URL /favorites, ao clicar no link Favorite Pokémon da barra de navegação', () => {
-    const { history } = renderWithRouter(<App />);
-
-    const favorite = screen.getByRole('link', { name: 'Favorite Pokémon' });
-    userEvent.click(favorite);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/favorites');
-  });
-
-  it('Testa se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
-    const { history } = renderWithRouter(<App />);
-
-    act(() => {
-      history.push('/twich');
-    });
-
-    const notFoundTitle = screen.getByRole('heading', { name: 'Page requested not found' });
-    expect(notFoundTitle).toBeInTheDocument();
+    expect(screen.getByRole('img', '')).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
