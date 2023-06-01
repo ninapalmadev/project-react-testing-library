29d576009d8e37ef68bd7a556b732cb208c3d709
Nina Palma <ninapalmadev@gmail.com>
2023-06-01 13:35:10 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <NotFound.js />', () => {
+  it('Testa se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Testa se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFound = screen.getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+    expect(notFound).toBeInTheDocument();
+    expect(notFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
